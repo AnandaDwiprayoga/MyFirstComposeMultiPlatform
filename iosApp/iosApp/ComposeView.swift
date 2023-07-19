87ad71bb53676988da39d09bef782310ea357093
Ananda Dwi Prayoga <prayogaananda04@gmail.com>
2023-07-19 12:58:06 +0700

    Implement shared view CounterPage.kt on iOS side

---
+++ iosApp/iosApp/ComposeView.swift
@@ -0,0 +1,19 @@
+//
+//  ComposeView.swift
+//  iosApp
+//
+//  Created by Mac on 19/07/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import shared
+
+struct ComposeView: UIViewControllerRepresentable {
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+}
---
+++ shared/src/iosMain/kotlin/com/example/myfirstkmm/MainViewController.kt
@@ -0,0 +1,6 @@
+package com.example.myfirstkmm
+
+import androidx.compose.ui.window.ComposeUIViewController
+import com.example.myfirstkmm.compose.CounterPage
+
+fun MainViewController() = ComposeUIViewController { CounterPage() }
