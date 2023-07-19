5cdc86eb8bc8df06e8ca8a7087afd9933704dea4
Ananda Dwi Prayoga <prayogaananda04@gmail.com>
2023-07-19 08:26:10 +0700

    init kmm

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyFirstKmm"
+include(":androidApp")
+include(":shared")
